744d3c67384f86639548bbf6d913d49ef10b0a97
Darryl Johnson <dandycodes@gmail.com>
2022-09-13 20:09:14 -0400

    code issues on asset management 9/13

--- public/js/budget.js
+++ public/js/budget.js
@@ -1,11 +1,12 @@
 const balance = document.getElementById('balance');
 const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
-const asset = document.getElementById('money-asset')
 const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
+const text1 = document.getElementById('text1');
 const amount = document.getElementById('amount');
+const amount1 = document.getElementById('amount1');
 
 
 
@@ -26,10 +27,13 @@ function addTransaction(e) {
     const transaction = {
       id: generateID(),
       text: text.value,
-      amount: +amount.value
+      amount: +amount.value,
+      text1: text1.value,
+      amount1: +amount1.value
     };
 
     transactions.push(transaction);
+  
 
     addTransactionDOM(transaction);
 
@@ -38,6 +42,8 @@ function addTransaction(e) {
     updateLocalStorage();
 
     text.value = '';
+    text1.value = '';
+    amount1.value = '';
     amount.value = '';
   }
 }
