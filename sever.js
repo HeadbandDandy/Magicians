db3960fff244ce8b5f303522e1c2707492e115e7
JioR95 <jiovani.rivera@rocketmail.com>
2022-09-15 10:55:03 -0400

    app.use

--- sever.js
+++ sever.js
@@ -7,3 +7,19 @@ const mysql = require('mysql');
 const crypto = require('crypto');
 var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
+
+app.use(session({
+    key: 'session_cookie_name' ,
+    secret: 'session_cookie_secret' ,
+    store: new MySQLStore({
+        host: 'localhost' ,
+        port: 3306,
+        user: 'root' ,
+        database: 'cookie_user'
+     }),
+     resave: false,
+     saveUninitialized: false,
+     cooke:{
+        maxAge:1000*60*60*24,
+     }
+}));
