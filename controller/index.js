895a33094e57a6f1914537002edc9950f6296465
brian nelson <bn3l10@gmail.com>
2022-09-15 23:03:04 -0400

    setup connection

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,7 @@
 //this file should contain the necessary items/modules in order to connect routes/router
 
-const router  = //
+const router = require('express').Router();
+
+
+
+module.exports = router;
