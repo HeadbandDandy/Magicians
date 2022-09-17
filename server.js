3f9501d1dd70ba08f281f9f2da8e6da41cf2ff73
Darryl Johnson <dandycodes@gmail.com>
2022-09-17 15:18:27 -0400

    updated server file 9/17

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: '',
@@ -26,7 +26,7 @@ const sess = {
 
 app.use(session(sess));
 
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const hbs = exphbs.create({ helpers });
 
