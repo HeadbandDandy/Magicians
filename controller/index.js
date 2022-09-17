51bfaa9ba63ef72ddb0e5ae6bc80c34c0feba511
Darryl Johnson <dandycodes@gmail.com>
2022-09-17 15:22:14 -0400

    index.js api file edited 9/17

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,9 @@
 //this file should contain the necessary items/modules in order to connect routes/router
 
-const router  = //
+const router = require('express').Router();
+
+const budgetRoutes = require('./budget-routes');
+
+router.use('/budgets', budgetRoutes);
+
+module.exports = router;
