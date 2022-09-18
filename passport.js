b91976aa3c8b7535b7916f319cd0eb6f3652dee1
Darryl Johnson <dandycodes@gmail.com>
2022-09-17 22:03:57 -0400

    passport file completed 9/17

--- passport.js
+++ passport.js
@@ -1,7 +1,8 @@
 //below contains authentication to run Passport.Js
 
 const passport = require("passport");
-const { ConnectionAcquireTimeoutError } = require("sequelize")
+const { ConnectionAcquireTimeoutError } = require("sequelize");
+const { resourceLimits } = require("worker_threads");
 
 
 
@@ -37,7 +38,61 @@ const verifyCallback = (username, password, done) => {
 const strategy = new LocalStrategy(customFields, verifyCallback);
 passport.use(strategy);
 
+
+//below will serialize a user ID
 passport.serializeUser((user, done) => {
     console.log('inside serialize');
     done(null, user.id)
 })
+
+
+//below will deserialize a user ID
+
+passport.deserializeUser(function(userId, done) {
+    console.log('deserializeUser' + userId);
+    connection.query('SELECT * FROM users where id = ?', [userId], function(error, results){
+        done(null, results[0])
+    })
+})
+
+//functions as middleware below
+function validPassword(password, hash, salt)
+{
+    var hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 60, 'sha512').toString('hex');
+    return hash === hashVerify;
+}
+function genPassword(password)
+{
+    var salt = crypto.randomBytes(32).toString('hex');
+    var genhash = crypto.pbkdf2Sync(password, salt, 10000, 60, 'sha512').toString('hex');
+    return {salt:salt, hash:genhash}
+}
+
+
+//redirects to notauthorized
+
+function isAuthorized(req, res, next) 
+{
+    if(req.isAuthenticated())
+    {
+        next();
+    }
+    else
+    {
+        res.redirect('/notAuthorized')
+    }
+}
+
+// checks if the user is an administrator
+function isAdmin(req, res, next) 
+{
+    if(req.isAuthenticated() && req.userf.isAdmin == 1)
+    {
+        next();
+    }
+    else
+    {
+        res.redirect('/notAuthorizedAdmin')
+    }
+}
+
