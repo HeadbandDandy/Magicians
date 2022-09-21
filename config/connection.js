32f0199b653ffbdb106d3111e2d693cdda244c7f
JioR95 <jiovani.rivera@rocketmail.com>
2022-09-20 22:01:55 -0400

    .env password

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-require('dotenv-safe').config({allowEmptyValues: false});
+require('dotenv').config();
 
 
 
@@ -8,9 +8,7 @@ const sequelize = process.env.JAWSDB_URL
     ? new Sequelize(process.env.JAWSDB_URL)
     : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: 'dgb_login',
+        dialect: 'mysql',
         port: 3306
     });
 
