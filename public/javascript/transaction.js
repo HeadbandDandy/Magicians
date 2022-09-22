0c78500112249bc492a9eec77027d458c75efa59
brian nelson <bn3l10@gmail.com>
2022-09-22 18:26:13 -0400

    balance

--- public/javascript/transaction.js
+++ public/javascript/transaction.js
@@ -26,5 +26,5 @@ async function transactionFormHandler(event) {
     }
   
   
-  document.querySelector('.transaction-form').addEventListener('submit', transactionFormHandler);
+  document.querySelector('transactionForm').addEventListener('submit', transactionFormHandler);
   
